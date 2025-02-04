7096b5a73ca1c121837a43310a48013d65019e71
Pierce Casey <ppj679o@gmail.com>
2025-02-04 18:06:13 -0500

    Initial environment and backend notes

--- backend/test.py
+++ backend/test.py
@@ -1,1 +1,20 @@
+import sklearn
+import numpy as np
+import pandas as pd
 
+print("Hello World!")
+
+#dataFrame = pd.loaddata.here
+
+"""Potential Models:
+
+Linear (Could be depreciated by end)
+Random Forest
+Singular Value Decomposition
+Gradient Boost (or any boosting algorithm)
+Neural Network
+
+"""
+
+pm1 = sklearn.linear_model.LinearRegression()
+#pm1 = pm1.fit(dataFrame)
