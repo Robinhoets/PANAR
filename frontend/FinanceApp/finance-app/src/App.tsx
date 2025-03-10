5195edff7f7f97df04f469a26e107bdb939d6bbd
Amari Nixon <101675831+AmariN12@users.noreply.github.com>
2025-03-10 02:03:59 -0400

    Start page and Ticker entry setup

--- frontend/FinanceApp/finance-app/src/App.tsx
+++ frontend/FinanceApp/finance-app/src/App.tsx
@@ -1,35 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState, useEffect } from 'react';
+
+function Ticker_Enter_Page_Form() {
+    const [inputs, setInputs] = useState({
+        ticker : "",
+        model : "",
+    });
+  
+    const handleChange = (event : any) => {
+      const name = event.target.name;
+      const value = event.target.value;
+      setInputs(values => ({...values, [name]: value}))
+    }
+  
+    const handleSubmit = (event : any) => {
+      event.preventDefault();
+      //alert(inputs);
+    }
+  
+    return (
+    <div style={{scale: 3}}>
+        <form onSubmit={handleSubmit}>
+            <label>Enter the company ticker:&nbsp;
+            <input 
+            type="text" 
+            name="ticker" 
+            value={inputs.ticker || ""} 
+            onChange={handleChange}
+            required
+            />
+            </label>
+            <br></br>
+            <label>Select the model:<br></br>
+                <input type="radio" id="m1" name="model" value="Model 1" onChange={handleChange} required></input>
+                <label htmlFor="m1">Model 1</label><br></br>
+                <input type="radio" id="m2" name="model" value="Model 2" onChange={handleChange} required></input>
+                <label htmlFor="m2">Model 2</label><br></br>
+                <input type="radio" id="m3" name="model" value="Model 3" onChange={handleChange} required></input>
+                <label htmlFor="m3">Model 3</label>
+            </label>
+            <br></br>
+            <input type="submit" />
+        </form>
+    </div>
+    )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [pageIndex, setPageIndex] = useState<number>(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const Pages = Object.freeze({
+        Start_Page: 0,
+        Ticker_Enter_Page: 1,
+        User_Select_Page: 2,
+    });
+
+    useEffect(() => {
+        setPageIndex(JSON.parse(window.localStorage.getItem('pageIndex') || '{}'));
+      }, []);
+    
+    useEffect(() => {
+    window.localStorage.setItem('pageIndex', String(pageIndex));
+    }, [pageIndex]);
+
+    switch(pageIndex){
+        case Pages.Start_Page:
+            return(
+                <div>
+                    <div className="header">
+                        <h1>PANAR Financial Modeling App</h1>
+                    </div>
+                    <button 
+                    className="startBtn"
+                    onClick={() => setPageIndex(Pages.Ticker_Enter_Page)}
+                    >
+                        Start
+                    </button>
+                </div>
+            );
+        case Pages.Ticker_Enter_Page:
+            return(
+                <div>
+                    <div>
+                        <Ticker_Enter_Page_Form />
+                    </div>
+                </div>
+            );
+    }
 }
 
 export default App
