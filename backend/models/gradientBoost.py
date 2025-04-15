8f15269fe41d9afdb1723d1dd2fcaa83172b07df
Pierce Casey <ppj679o@gmail.com>
2025-04-14 21:33:31 -0400

    working model update

--- backend/models/gradientBoost.py
+++ backend/models/gradientBoost.py
@@ -1,4 +1,4 @@
-from sklearn.ensemble import GradientBoostingRegressor
+import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.impute import SimpleImputer
@@ -7,50 +7,83 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 import numpy as np
 import pandas as pd
-import joblib
+import matplotlib.pyplot as plt
+from xgboost import plot_tree
 
-"""Potential Models:
+print(xgb.__version__)
+print(xgb.rabit.get_rank())
 
-Linear (Could be depreciated by end)
-Random Forest
-Singular Value Decomposition
-Gradient Boost (or any tree boosting algorithm)
-Neural Network
+dataFrame = pd.read_csv("statement.csv")
+data_columns = dataFrame[["revenue", "cogs", "gross_profit", "operating_expenses", "net_income"]]
 
-"""
+#Find correlation matrix of data points
+corrMatrix = dataFrame[["revenue", "cogs", "gross_profit", "operating_expenses", "net_income"]].corr()
+print(corrMatrix["net_income"].sort_values(ascending=False))
 
-dataFrame = pd.loaddata.here
-data_columns = dataFrame[['all', 'input', 'columns', 'here']]
+#Create new year column for encoding
+dataFrame["year_cat"] = dataFrame["end_date"].astype(str).str[:4]
 
-#Standard imputer to remove potential NaN and scaler for numerical values 
+#pipe = Pipeline([
+#    ('imputer', SimpleImputer()), 
+#    ('std_scaler', StandardScaler())])
+
 pipe = Pipeline([
-    ('imputer', SimpleImputer()), 
-    ('std_scaler', StandardScaler())])
+    ('imputer', SimpleImputer())])
 
 #Potential one hot encoding transformer to be added (if categorical data required)
-#ohe = ColumnTransformer([
-#    ("num", pipe, list(data_columns)),
-#    ("cat", OneHotEncoder(), ['insert', 'categories', 'here'])
-#])
+ohe = ColumnTransformer([
+    ("num", pipe, list(data_columns)),
+    ("cat", OneHotEncoder(), ["quarter_list", "year_cat"])
+])
 
-data_prep = pipe.fit_transform(dataFrame)
-#data_prep = ohe.fit_transform(dataFrame)
+data_prep = ohe.fit_transform(dataFrame)
 
-data_train, data_test = train_test_split(data_prep, test_size=0.2)
-train_target = data_train['target']
-test_target = data_test['target']
+#MUST PREDICT NEXT VALUE. CAN NOT SHUFFLE.
+#data_train, data_test = train_test_split(data_prep, test_size=0.2)
+data_train = data_prep[:32]
+data_test = data_prep[32:]
 
-#Gradient Boost Regression; max_features = columns to consider, max_depth = levels of decision tree to use  
-gBoost = GradientBoostingRegressor(n_estimators=500, max_features=5, max_depth=5)
-gBoost.fit(data_train, train_target)
+data_train = data_train[:-1, :]
+data_test = data_test[:-1, :]
+train_target = data_train[:, [1, 2, 3, 4, 5]]
+test_target = data_test[:, [1, 2, 3, 4, 5]]
 
-y_result = gBoost.predict(data_test, test_target)
 
-#Determine RSME and R2 score to judge model performance
-performance = mean_squared_error(test_target, y_result)
-print("Gradient Boost RMSE: ", np.sqrt(performance))
-r2 = r2_score(y_result, test_target)
+print(data_train.shape[0])
+print(train_target.shape[0])
+print(data_test.shape)
+print(test_target.shape)
+
+#Get data into xgBoost form
+dTrain = xgb.DMatrix(data_train, train_target)
+dTest = xgb.DMatrix(data_test, test_target)
+
+#TODO: MUST FIND BEST PARAMETERS 
+parameters = {
+    'objective': 'reg:squarederror',
+    'device': 'cuda',
+    'eval_metric': 'rmse',
+    'learning_rate': 0.1,
+    'max_depth': 4
+}
+
+boost = xgb.train(parameters, dTrain)
+yPred = boost.predict(dTest)
+
+plot_tree(boost)
+plt.show()
+
+performance = mean_squared_error(test_target, yPred)
+print("RMSE: ", np.sqrt(performance))
+r2 = r2_score(test_target, yPred)
 print("R2 score = ", r2)
 
-#Save model with joblib
-#joblib.dump(gBoost, "Gradient_Boost_PANAR_Model")
+boost.save_model("BOOST_WORKING.model")
+#boost.dump_model('dump.raw.txt', 'featmap.txt')
+
+#Reload model
+#boost = xgb.Booster({'nthread': 4})  
+#boost.load_model('BOOST_97.model') 
+
+yUser = boost.predict(dTest)
+print(yUser)
